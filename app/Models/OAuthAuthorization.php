d2f08d724dcd039958bccb49d7ba5c3ec324e7da
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-04-08 16:37:50 +0200

    migrations and models made it

---
+++ app/Models/OAuthAuthorization.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OAuthAuthorization extends Model
+{
+    use HasFactory;
+}
